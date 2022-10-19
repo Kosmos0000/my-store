941d85ed2be6cd1321421a44d588a4b48368dd94
the.rus.p@yandex.ru <the.rus.p@yandex.ru>
2022-10-19 08:42:24 +0300

    added sizes

--- src/components/productItem/productItem.jsx
+++ src/components/productItem/productItem.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './productItem.module.css';
 import {useDispatch} from "react-redux";
-import {addToFavorites, removeToFavorites} from "../../redux-tollkit/redusers/goods-reducer";
+import {
+    addToBasket,
+    addToFavorites,
+    removeToFavorites
+} from "../../redux-tollkit/redusers/goods-reducer";
+import heart from './icons/Heart.svg';
+import heartActive from './icons/HeartActive.svg';
+import basket from './icons/Basket.svg';
 
 
 const ProductItem = ({item}) => {
@@ -12,25 +19,39 @@ const ProductItem = ({item}) => {
 
     let dispatch = useDispatch();
 
+    let [editMode, setEditMode] = useState(false);
+
+    let sizes = ['XXS/30', 'XS/32', 'M/30', 'L/42'].map((size) =>
+        item.sizes.includes(size, 0) ?
+        <span className={style.size + ' ' + style.active}>   {size}   </span> : <span className={style.size}>   {size}   </span>
+    );
+
 
 
     return (
-        <div className={style.product}>
+
+        <div onMouseOver={() => setEditMode(true)} onMouseOut={() => setEditMode(false)} className={style.product}>
             <div style={styleImage} className={style.image}>
                 {item.discount > 0 && <span className={style.percent}>{item.discount} %</span>}
-                {item.isInFavorites ? <img onClick={() => dispatch(removeToFavorites(item.id))} className={style.icon} src="https://www.svgrepo.com/show/21103/heart.svg" alt="heart"/> :
-                    <img onClick={() => dispatch(addToFavorites(item.id))} className={style.icon} src="https://www.svgrepo.com/show/5233/heart.svg" alt="heart"/>
+                {item.isInFavorites ? <img onClick={() => dispatch(removeToFavorites(item.id))} className={style.icon}
+                                           src={heartActive} alt="heart"/> :
+                    <img onClick={() => dispatch(addToFavorites(item.id))} className={style.icon}
+                         src={heart} alt="heart"/>
                 }
-            </div>
-            <div>
-                {item.discount > 0 ?
-                    <div className={style.priceContainer}>
-                        <div className={style.price}>{item.price}</div>
-                        <div className={style.discountPrice}>{item.price * item.discount / 100} грн</div>
-                    </div> : <div className={style.priceContainer}>
-                        <div style={{textDecoration: 'none'}} className={style.price}>{item.price} грн</div>
+                <div className={style.productInfo}>
+                    {item.discount > 0 ?
+                        <div className={style.priceContainer}>
+                            <div className={style.price}>{item.price}</div>
+                            <div className={style.discountPrice}>{item.price * item.discount / 100} грн</div>
+                        </div> : <div className={style.priceContainer}>
+                            <div style={{textDecoration: 'none'}} className={style.price}>{item.price} грн</div>
+                        </div>}
+                    {editMode && <div className={style.sizeAndBasket}>
+                        <div>{sizes}</div>
+                        <div onClick={() => dispatch(addToBasket(item.id))} className={style.basket}>В корзину <img src={basket} alt=""/></div>
                     </div>}
 
+                </div>
             </div>
         </div>
     );
--- src/redux-tollkit/redusers/goods-reducer.js
+++ src/redux-tollkit/redusers/goods-reducer.js
@@ -3,12 +3,14 @@ import {createReducer, createAction} from "@reduxjs/toolkit";
 
 export const addToFavorites = createAction('ADD_TO_FAVORITES');
 export const removeToFavorites = createAction('REMOVE_TO_FAVORITES');
+export const addToBasket = createAction('ADD_AND_REMOVE_TO_BASKET');
+export const removeToBasket = createAction('REMOVE_TO_BASKET');
 
 const initialState = {
     items : [
         {
             id: 1,
-            src: 'https://fashionactualtrends.com/userfiles/196/3077_11.webp',
+            src: 'https://avatars.mds.yandex.net/get-mpic/5221441/img_id5252206666447556010.jpeg/orig',
             price: 2799,
             discount: 70,
             colors: ['Beige', 'Black', 'White'],
@@ -18,27 +20,27 @@ const initialState = {
         },
         {
             id: 2,
-            src: 'http://i1.adis.ws/i/boohooamplience/fzz94269_chartreuse_xl.jpg',
+            src: 'https://imgcdn.zarina.ru/upload/images/22644/thumb/450_9999/2264402102_133_1.jpg?t=1648047868',
             price: 3500,
             discount: 0,
             colors: ['Beige', 'Black'],
-            sizes: ['XXS/30', 'XS/32', 'M/30', 'L/42',],
+            sizes: ['XXS/30', 'M/30', 'L/42',],
             isInFavorites: false,
             isItInTheBasket: false
         },
         {
             id: 3,
-            src: 'https://imgaz1.chiccdn.com/thumb/large/oaupload/newchic/images/AE/AE/2901c90f-ace2-4d86-aa87-65307483cb41.jpg?s=702x936',
+            src: 'https://i.pinimg.com/736x/18/5f/99/185f99206229dc866511aa5333a218e0.jpg',
             price: 3400,
             discount: 50,
-            colors: ['Black', 'White'],
+            colors: ['Black'],
             sizes: ['XXS/30', 'XS/32', 'M/30', 'L/42',],
             isInFavorites: false,
             isItInTheBasket: false
         },
         {
             id: 4,
-            src: 'https://static.rasprodaga.ru/pictures/goods/mainPage/514487875/3053606/5160/7992dzz26983_candy_20floss_xl.maybe.jpg',
+            src: 'https://zolla.com/upload/iblock/416/a780edhrh8zt515liaa6codbwfw1j6v2.jpg',
             price: 3000,
             discount: 40,
             colors: ['White'],
@@ -55,5 +57,11 @@ export default createReducer(initialState, {
     },
     [removeToFavorites]: function (state, action) {
         state.items = state.items.map((item) => item.id === action.payload ? {...item, isInFavorites: false} : {...item} );
+    },
+    [addToBasket]: function (state, action) {
+        state.items = state.items.map((item) => item.id === action.payload ? {...item, isItInTheBasket: true} : {...item} );
+    },
+    [removeToBasket]: function (state, action) {
+        state.items = state.items.map((item) => item.id === action.payload ? {...item, isItInTheBasket: false} : {...item} );
     }
 })
